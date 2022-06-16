5a6172dd16697bae110fd44fbf63b11790d9da6f
sumanta2 <sumantagorai.21@gmail.com>
2022-06-16 13:48:38 +0530

    9 vdo10 pass different type in useState

---
+++ src/components/State/User.tsx
@@ -0,0 +1,30 @@
+import {useState}  from 'react'
+
+type AuthUser = {
+    name:string
+    email: string
+}
+
+const User = () => {
+    //in useState <AuthUser | null> indicate later useState store AuthUser or null type Variable/values
+    const [user,setUser]= useState<AuthUser | null>(null)
+    const handleLogin = () =>{
+        setUser(
+            {
+                name:"father",
+                email:"father.example.com"
+            }
+        )
+    }
+    const handleLogout = () => {}
+  return (
+    <div>
+         <button onClick={handleLogin}>Login</button>
+        <button onClick={handleLogout}>LogOut</button>
+        <div>User name is {user?.name}</div>   {/*   here data type may be null so here optional chaining are used*/}
+        <div>User email is {user?.email}</div>
+    </div>
+  )
+}
+
+export default User
