ad049ea17f5a2cf60e0967931c033f8ab11d5020
sumanta2 <sumantagorai.21@gmail.com>
2022-06-18 16:53:07 +0530

    17 vdo18 some Component Props types

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Please login to continue</div>
+  )
+}
+
+export default Login
---
+++ src/components/auth/Private.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from './Login'
+import {ProfileProps} from './Profile'
+type PrivateProps={
+    isLoggedIn:boolean 
+    component:React.ComponentType<ProfileProps>
+}
+export const Private = ({isLoggedIn,component:Component}:PrivateProps) => {
+    if(isLoggedIn){
+        return <Component name="Reading"/>
+    }
+    else{
+        return <Login/>
+    }
+  return (
+    <div>Private</div>
+  )
+}
+     
